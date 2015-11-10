3d312648897d7fc7035d724973913effc587929a
dotlouis <1891109+dotlouis@users.noreply.github.com>
2015-11-10 15:20:55 +0100

    Added countdown

--- assets/js/perso.js
+++ assets/js/perso.js
@@ -1,27 +1,59 @@
 $(function(){
 
-count();
+    var deadline;
 
-// on répète la fonction toutes les secondes
-setInterval(count, 1000);
+    $('.start').click(function(){
+        deadline = $('.date').val();
+        setInterval(countDown, 1000);
+    });
 
-function count(){
-    // on créé une variable pour stocker l'information
-    var now = new Date();
-    var hour = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
+    function countDown(){
+        var remainingTime = getRemainingTime(deadline);
 
-    // pour voir les variables
-    console.log(now);
-    console.log(hour, minutes, seconds);
+        var days = remainingTime.days;
+        var hours = remainingTime.hours;
+        var minutes = remainingTime.minutes;
+        var seconds = remainingTime.seconds;
 
-    // on créé le texte a mettre dans le html
-    var counter = hour + 'h ' + minutes + 'm ' + seconds + 's';
-    console.log(counter);
+        var counter = days + 'd '+ hours + 'h ' + minutes + 'm ' + seconds + 's';
 
-    // on le remplace dans le HTML
-    $('.counter').html(counter);
-}
+        $('.counter').html(counter);
+    }
+
+    setInterval(count, 1000);
+
+    function count(){
+        // on créé une variable pour stocker l'information
+        var now = new Date();
+        var hour = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
+
+        // pour voir les variables
+        // console.log(now);
+        // console.log(hour, minutes, seconds);
+
+        // on créé le texte a mettre dans le html
+        var counter = hour + 'h ' + minutes + 'm ' + seconds + 's';
+        // console.log(counter);
+
+        // on le remplace dans le HTML
+        $('.counter').html(counter);
+    }
+
+    function getRemainingTime(endtime){
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor( (t/1000) % 60 );
+        var minutes = Math.floor( (t/1000/60) % 60 );
+        var hours = Math.floor( (t/(1000*60*60)) % 24 );
+        var days = Math.floor( t/(1000*60*60*24) );
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 
 });
